4e6ffb43b5aea44adad10f1d971df711bba8d4ef
Shivangi Sharma <shanussh@gmail.com>
2026-02-25 16:55:14 +0530

    image from ecr

--- practice_backend/practice_backend/practice_backend_stack.py
+++ practice_backend/practice_backend/practice_backend_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
     aws_apigateway as apigateway,
+    aws_ecr as ecr,
 )
 from constructs import Construct
 
@@ -11,16 +12,22 @@ class PracticeBackendStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
-        lambda_function = _lambda.Function(
-        self,
-        "MyLambda",
-        runtime=_lambda.Runtime.PYTHON_3_11,
-        handler="handler.lambda_handler",
-        code=_lambda.Code.from_asset("lambdas"),
-)
+        repository = ecr.Repository.from_repository_name(
+            self,
+            "PracticeLambdaRepo",
+            "practice-lambda"
+        )
+
+        lambda_function = _lambda.DockerImageFunction(
+            self,
+            "DockerLambda",
+            code=_lambda.DockerImageCode.from_ecr(
+            repository,
+            tag_or_digest="latest"),
+        )
 
         apigateway.LambdaRestApi(
             self,
-            "MyApi",
+            "DockerApi",
             handler=lambda_function,
         )
